85263077f79ec84875f291ac40ffebe2b86ecab1
muhharuntahir <muhharuntahir@gmail.com>
2024-05-13 11:37:48 +0800

    setup dashboard fe

---
+++ src/components/flowbite-wrapper.tsx
@@ -0,0 +1,26 @@
+import { Flowbite, useThemeMode } from "flowbite-react";
+import type { FC } from "react";
+import { useEffect } from "react";
+import { Outlet } from "react-router";
+import theme from "../flowbite-theme";
+
+const FlowbiteWrapper: FC = function () {
+  return (
+    <Flowbite theme={{ theme }}>
+      <PersistFlowbiteThemeToLocalStorage />
+      <Outlet />
+    </Flowbite>
+  );
+};
+
+const PersistFlowbiteThemeToLocalStorage: FC = function () {
+  const [themeMode] = useThemeMode();
+
+  useEffect(() => {
+    localStorage.setItem("theme", themeMode);
+  }, [themeMode]);
+
+  return <></>;
+};
+
+export default FlowbiteWrapper;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,37 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { StrictMode, useEffect } from "react";
+import { Flowbite, useThemeMode } from "flowbite-react";
+import theme from "../flowbite-theme";
+import { Inter } from "next/font/google";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+// const roboto = Roboto({ subsets: ["latin"], weight: ["400", "700"] });
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["300", "400", "500", "600", "700"],
+});
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  // const dark = localStorage.getItem("theme") === "dark";
+  const [themeMode] = useThemeMode();
+
+  useEffect(() => {
+    localStorage.setItem("theme", themeMode);
+  }, [themeMode]);
+
+  return (
+    <div className={inter.className}>
+      <div className="bg-gray-50 dark:bg-gray-900 antialiased">
+        <Flowbite theme={{ theme }}>
+          <StrictMode>
+            <Component {...pageProps} />
+          </StrictMode>
+        </Flowbite>
+      </div>
+    </div>
+  );
 }
